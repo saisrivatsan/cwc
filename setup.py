c4fc0ac47cd3247d5464471ecf677d451b499983
root <saisrivatsan12@gmail.com>
2021-12-12 02:31:25 +0000

    made changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 # System imports
 from distutils.core import *
-from distutils      import sysconfig
+from distutils import sysconfig
 import glob
 
 # Third-party modules - we depend on numpy for everything
@@ -21,10 +21,10 @@ srcDir = os.path.abspath('src')
 for root, dirnames, filenames in os.walk(srcDir):
   for dirname in dirnames:
     absPath = os.path.join(root, dirname)
-    print('adding dir to path: %s' % absPath)
+    #print('adding dir to path: %s' % absPath)
     globStr = "%s/*.c*" % absPath
     files = glob.glob(globStr)
-    print(files)
+    #print(files)
     includeDirs.append(absPath)
     srcFiles += files
 
@@ -33,25 +33,18 @@ print(includeDirs)
 print("srcFiles:")
 print(srcFiles)
 
-# set the compiler flags so it'll build on different platforms (feel free
-# to file a  pull request with a fix if it doesn't work on yours)
-if sys.platform == 'darwin':
-    # default to clang++ as this is most likely to have c++11 support on OSX
-    if "CC" not in os.environ or os.environ["CC"] == "":
-        os.environ["CC"] = "clang++"
-        # we need to set the min os x version for clang to be okay with
-        # letting us use c++11; also, we don't use dynamic_cast<>, so
-        # we can compile without RTTI to avoid its overhead
-        extra_args = ["-stdlib=libc++",
-          "-mmacosx-version-min=10.7","-fno-rtti",
-          "-std=c++0x"]  # c++11
-else: # only tested on travis ci linux servers
-    os.environ["CC"] = "g++" # force compiling c as c++
-    extra_args = ['-std=c++0x','-fno-rtti']
+
+os.environ["CC"] = "g++" # force compiling c as c++
+extra_args = ['-std=c++14','-fno-rtti','-march=native', '-ffast-math']
+extra_args += ['-stdlib=libc++']
+os.environ['CC'] = "clang"
+os.environ['CXX'] = "clang++"
+os.environ['LDFLAGS'] = '-lc++'
 
 # inplace extension module
 _example = Extension("_example",
                    srcFiles,
+                   define_macros=[('NDEBUG', '1')],
                    include_dirs=includeDirs,
                    swig_opts=['-c++'],
                    extra_compile_args=extra_args,
